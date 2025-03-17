eb9276d4bb575bb9f9beb11645770923db38cbee
XArcanaX <deramane.waren01@gmail.com>
2025-03-17 11:08:41 +1100

    Premier commit du projet Cartographie du risque de Leptospirose en Nouvelle-Calédonie : Interfaces Graphiques.

---
+++ src/components/header.jsx
@@ -0,0 +1,41 @@
+import  React from 'react'
+import { Link } from 'react-router-dom';
+import ThemeToggle from './ThemeToggle';
+
+const Header = ({ showSourceSelector = false, title = null }) => {
+    return (
+        <header>
+            <section>
+                <ThemeToggle />
+                <nav>
+                    <Link className="nav-link" to="/">Accueil</link>
+                    <Link className="nav-link" to="/DataDescription">Description des cas</link>
+                    <Link className="nav-link" to="/Predictions">Prédiction des cas</link>
+                    <Link className="nav-link" to="/About">À propos</link>
+                </nav>
+
+                {showSourceSelector && (
+                    <div id="menu1">
+                        <div className="menu-source1">
+                        <select id="source-select1">
+                            <option value="georep.nc">Georep.nc</option>
+                            <option value="oeil.nc">Oeil.nc</option>
+                            <option value="source3">Source 3</option>
+                        </select>
+                        </div>
+                        
+                        <div className="menu-fichier1">
+                        <select id="gpkg-select">
+                            <option>Une option</option>
+                        </select>
+                        </div>
+                    </div>
+                )}
+                {title && <h2>{title}</h2>}
+            </section>
+
+        </header>
+    );
+};
+
+export default Header;
